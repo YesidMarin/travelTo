8811216fbf259dcaa6dfb53c57a3660c3f7f0742
Vargas Gaitan <rvargas@LocatelColombia.com>
2019-05-31 07:31:28 -0500

    Validacion de cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalPageComponent } from './principal-page/principal-page.component';
-import { Page404Component } from './page404/page404.component';
-
+  
 const routes: Routes = [
-  {path: '',  component:PrincipalPageComponent, pathMatch: 'full'},
-  {path: '**', component:Page404Component}
+  {path: '',  redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: PrincipalPageComponent},
 ];
 
 @NgModule({
