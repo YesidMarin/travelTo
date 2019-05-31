e069dc85743479d0a5acc5d4fedca9178441a878
Yesid Marin <yesidmarin.dev@gmail.com>
2019-05-31 11:46:39 -0500

    add styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalPageComponent } from './principal-page/principal-page.component';
-  
+import { Page404Component } from './page404/page404.component';
+
 const routes: Routes = [
-  {path: '',  redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: PrincipalPageComponent},
+  {path: '',  component:PrincipalPageComponent, pathMatch: 'full'},
+  {path: '**', component:Page404Component}
 ];
 
 @NgModule({
